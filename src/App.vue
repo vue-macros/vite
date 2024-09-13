99aecd26fefe415b84721e5fa7cc288bb8321841
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2024-09-13 19:31:28 +0800

    chore: upgrade deps

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,12 @@
 <script setup lang="ts">
 const foo = defineProp('foo', { default: 'foo' })
+
+let msg = $ref(10)
+msg++
 </script>
 
 <template>
   <h1>Hello, Vue Macros!</h1>
   {{ foo }}
+  {{ msg }}
 </template>
